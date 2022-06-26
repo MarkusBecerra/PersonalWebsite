8d1a209ec691fa042636b669498ccdbc7a7a683e
Markus Becerra <markusbecerra@ku.edu>
2022-06-25 22:50:54 -0400

    Init commit w/ WIP page.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title> About </title>
+            <h1> About! </h1>
+            <p> I am working on my personal website and blog at the moment! </p>
+        </main>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/wip.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const WIPPage = () => {
+    return (
+        <main>
+            <title> WIP </title>
+            <h1> Work In Progress! </h1>
+            <p> Apologies for the lack of excitment. Currently working on the website. Come back soon :) </p>
+        </main>
+    )
+}
+
+export default WIPPage
